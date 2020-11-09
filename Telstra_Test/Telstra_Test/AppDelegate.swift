5e72dd61ab01a41909f4f08c6dde5af755c9cc64
Dinesh Reddy Chennuru <>
2020-11-09 17:58:12 +0530

    configured view controller as launching view instead of main storyboard.

--- Telstra_Test/Telstra_Test/AppDelegate.swift
+++ Telstra_Test/Telstra_Test/AppDelegate.swift
@@ -9,9 +9,15 @@ import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        let navController = UINavigationController(rootViewController: ViewController())
+        navController.navigationBar.prefersLargeTitles = true
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.rootViewController = navController
+        window?.makeKeyAndVisible()
         return true
     }
 
