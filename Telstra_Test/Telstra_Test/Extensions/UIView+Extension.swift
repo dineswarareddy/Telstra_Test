f4f87805fdf8662c18ec34a6f3c75d2905ec14a3
Dinesh Reddy Chennuru <>
2020-11-09 18:53:20 +0530

    Added uiview extension to add constraint to pin subview to super view.

---
+++ Telstra_Test/Telstra_Test/Extensions/UIView+Extension.swift
@@ -0,0 +1,18 @@
+//
+//  UIView+Extension.swift
+//  Telstra_Test
+//
+//  Created by Dinesh Reddy on 09/11/20.
+//
+
+import UIKit
+
+extension UIView {
+    func pin(to superView: UIView) {
+        translatesAutoresizingMaskIntoConstraints = false
+        topAnchor.constraint(equalTo: superView.topAnchor).isActive = true
+        leadingAnchor.constraint(equalTo: superView.leadingAnchor).isActive = true
+        bottomAnchor.constraint(equalTo: superView.bottomAnchor).isActive = true
+        trailingAnchor.constraint(equalTo: superView.trailingAnchor).isActive = true
+    }
+}
